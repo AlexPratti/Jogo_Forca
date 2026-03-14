c8c017b912a92444ecdbaba356ca78058b2d28e8
AlexPratti <prattiengenheiro@gmail.com>
2026-03-14 15:18:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ else:
     st.markdown("<h1 style='color:orange;'>JOGO DA FORCA</h1>", unsafe_allow_html=True)
 
     # Upload só aparece se jogador for Pratti
-     if st.session_state.jogador.lower() == "pratti":
+    if st.session_state.jogador.lower() == "pratti":
         arquivo = st.file_uploader("Carregue um arquivo Word (.docx)", type=["docx"])
         if arquivo:
             # Salva localmente
@@ -110,7 +110,7 @@ else:
             # Faz upload para Supabase
             with open("perguntas.docx", "rb") as f:
                 status = upload_arquivo(f)
-            if status == 200:
+                if status == 200:
                 st.success("Arquivo enviado com sucesso!")
             else:
                 st.error(f"Erro ao enviar arquivo. Código: {status}")
