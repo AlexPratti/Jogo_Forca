1c5f464712b8bd99b1169d80a76f3683e373d570
AlexPratti <prattiengenheiro@gmail.com>
2026-03-14 11:45:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -142,20 +142,6 @@ else:
         st.rerun()
 
     
-        if st.button("SAIR DO JOGO"):
-            del st.session_state["jogador"]
-            st.session_state.indice = None
-            st.session_state.acertos = 0
-            st.session_state.derrotas = 0
-            st.session_state.pergunta = None
-            st.session_state.palavra = None
-            st.session_state.letras_corretas = []
-            st.session_state.letras_erradas = []
-            st.session_state.erros = 0
-            st.session_state.fim_de_jogo = False
-            st.rerun()
-
-
         st.button("CORES LETRAS")
 
         if st.button("SAIR DO JOGO"):
