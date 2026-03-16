ea16aedcf99dff1f00069577044f4184a02606ff
AlexPratti <prattiengenheiro@gmail.com>
2026-03-15 21:17:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -292,11 +292,11 @@ else:
 
     carregar_perguntas()
 
-    if not st.session_state.pares:
-
-        st.warning("Nenhuma pergunta encontrada.")
-
-        st.stop()
+     if not st.session_state.pares:
+    
+        st.warning("Nenhuma pergunta encontrada. O administrador precisa enviar um arquivo.")
+    
+    else:
 
 # ================================
 # PLACAR
